5c15faf581ccf3ddd1308a103f209ac08e293f35
Moby <mobyvb@gmail.com>
2017-06-27 16:06:35 -0400

    restructure tests

--- test/index.js
+++ test/index.js
@@ -6,26 +6,35 @@ var expect = chai.expect;
 var libstorj = require('..');
 
 describe('libstorj', function() {
-
-  it('will give back timestamp', function() {
-    var timestamp = libstorj.util_timestamp();
-    expect(timestamp).to.be.a('number');
-    expect(timestamp).to.be.above(Date.now() - 1000);
-    expect(timestamp).to.be.below(Date.now() + 1000);
+  describe('#util_timestamp', function() {
+    it('will give back timestamp', function() {
+      var timestamp = libstorj.util_timestamp();
+      expect(timestamp).to.be.a('number');
+      expect(timestamp).to.be.above(Date.now() - 1000);
+      expect(timestamp).to.be.below(Date.now() + 1000);
+    });
   });
 
-  it('will check whether mnemonic is valid', function() {
-    var mnemonicCheckResult = libstorj.mnemonic_check('abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about');
-    expect(mnemonicCheckResult).to.equal(true);
+  describe('#mnemonic_check', function() {
+    it('should return true for a valid mnemonic', function() {
+      var mnemonicCheckResult = libstorj.mnemonic_check('abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about');
+      expect(mnemonicCheckResult).to.equal(true);
+    });
 
-    mnemonicCheckResult = libstorj.mnemonic_check('above winner thank year wave sausage worth useful legal winner thank yellow');
-    expect(mnemonicCheckResult).to.equal(false);
+    it('should return false for an invalid mnemonic', function() {
+      var mnemonicCheckResult = libstorj.mnemonic_check('above winner thank year wave sausage worth useful legal winner thank yellow');
+      expect(mnemonicCheckResult).to.equal(false);
+    });
 
-    mnemonicCheckResult = libstorj.mnemonic_check();
-    expect(mnemonicCheckResult).to.equal(false);
+    it('should return false if no argument is provided', function() {
+      var mnemonicCheckResult = libstorj.mnemonic_check();
+      expect(mnemonicCheckResult).to.equal(false);
+    });
 
-    mnemonicCheckResult = libstorj.mnemonic_check(5);
-    expect(mnemonicCheckResult).to.equal(false);
+    it('should return false for an argument that is not a string', function() {
+      var mnemonicCheckResult = libstorj.mnemonic_check(5);
+      expect(mnemonicCheckResult).to.equal(false);
+    });
   });
 
 });
