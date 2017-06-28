be8056e87d282694269070df141807557ee868ef
Moby <mobyvb@gmail.com>
2017-06-28 13:57:28 -0400

    add getInfo

--- libstorj.cc
+++ libstorj.cc
@@ -28,9 +28,66 @@ void MnemonicCheck(const FunctionCallbackInfo<Value>& args) {
     args.GetReturnValue().Set(mnemonic_check_result_local);
 }
 
+void GetInfoCb(uv_work_t *work_req, int status) {
+    json_request_t *req = (json_request_t *) work_req->data;
+
+    FunctionCallbackInfo<Value>& args = (FunctionCallbackInfo<Value>&) *req->handle;
+    Isolate *isolate = args.GetIsolate();
+
+    Local<Function> cb = Local<Function>::Cast(args[0]);
+
+    const char *result_str = json_object_to_json_string(req->response);
+
+    const unsigned argc = 2;
+    Local<Value> argv[argc] = {
+      Number::New(isolate, status),
+      JSON::Parse(String::NewFromUtf8(isolate, result_str))
+    };
+
+
+    cb->Call(Null(isolate), argc, argv);
+    free(req);
+    free(work_req);
+}
+
+void GetInfo(const FunctionCallbackInfo<Value>& args) {
+  storj_bridge_options_t bridge_options = {
+      .proto = "https",
+      .host  = "api.storj.io",
+      .port  = 443,
+      .user  = "testuser@storj.io",
+      .pass  = "dce18e67025a8fd68cab186e196a9f8bcca6c9e4a7ad0be8a6f5e48f3abd1b04"
+  };
+
+
+  storj_encrypt_options_t encrypt_options = {
+      .mnemonic = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about"
+  };
+
+  storj_http_options_t http_options = {
+      .user_agent = "storj-test",
+      .low_speed_limit = 0,
+      .low_speed_time = 0,
+      .timeout = 3
+  };
+
+  storj_log_options_t log_options = {
+      .level = 0
+  };
+
+  storj_env_t *env = storj_init_env(&bridge_options,
+                                    &encrypt_options,
+                                    &http_options,
+                                    &log_options);
+
+  storj_bridge_get_info(env, (void *) &args, GetInfoCb);
+  uv_run(env->loop, UV_RUN_DEFAULT);
+}
+
 void init(Handle<Object> exports) {
-    NODE_SET_METHOD(exports, "util_timestamp", Timestamp);
-    NODE_SET_METHOD(exports, "mnemonic_check", MnemonicCheck);
+    NODE_SET_METHOD(exports, "utilTimestamp", Timestamp);
+    NODE_SET_METHOD(exports, "mnemonicCheck", MnemonicCheck);
+    NODE_SET_METHOD(exports, "getInfo", GetInfo);
 }
 
 NODE_MODULE(bitcoinconsensus, init);
--- test/index.js
+++ test/index.js
@@ -8,7 +8,7 @@ var libstorj = require('..');
 describe('libstorj', function() {
   describe('#util_timestamp', function() {
     it('will give back timestamp', function() {
-      var timestamp = libstorj.util_timestamp();
+      var timestamp = libstorj.utilTimestamp();
       expect(timestamp).to.be.a('number');
       expect(timestamp).to.be.above(Date.now() - 1000);
       expect(timestamp).to.be.below(Date.now() + 1000);
@@ -17,24 +17,32 @@ describe('libstorj', function() {
 
   describe('#mnemonic_check', function() {
     it('should return true for a valid mnemonic', function() {
-      var mnemonicCheckResult = libstorj.mnemonic_check('abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about');
+      var mnemonicCheckResult = libstorj.mnemonicCheck('abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about');
       expect(mnemonicCheckResult).to.equal(true);
     });
 
     it('should return false for an invalid mnemonic', function() {
-      var mnemonicCheckResult = libstorj.mnemonic_check('above winner thank year wave sausage worth useful legal winner thank yellow');
+      var mnemonicCheckResult = libstorj.mnemonicCheck('above winner thank year wave sausage worth useful legal winner thank yellow');
       expect(mnemonicCheckResult).to.equal(false);
     });
 
     it('should return false if no argument is provided', function() {
-      var mnemonicCheckResult = libstorj.mnemonic_check();
+      var mnemonicCheckResult = libstorj.mnemonicCheck();
       expect(mnemonicCheckResult).to.equal(false);
     });
 
     it('should return false for an argument that is not a string', function() {
-      var mnemonicCheckResult = libstorj.mnemonic_check(5);
+      var mnemonicCheckResult = libstorj.mnemonicCheck(5);
       expect(mnemonicCheckResult).to.equal(false);
     });
   });
 
+  describe('#get_info', function() {
+    it('should get info about the bridge', function(done) {
+      libstorj.getInfo(function(status, result) {
+        expect(result.info.title).to.equal('Storj Bridge');
+        done();
+      });
+    });
+  });
 });
