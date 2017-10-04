477fd4f44966122629e0cd46118000465c58f3ac
Braydon Fuller <courier@braydon.com>
2017-10-04 01:26:24 -0400

    Move git clone to .travis.sh

--- .travis.sh
+++ .travis.sh
@@ -2,6 +2,11 @@
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew install curl nettle libmicrohttpd libuv
+    git clone https://github.com/Storj/libstorj.git
+    cd libstorj
+    ./autogen.sh
+    ./configure
+    sudo make install
 else
     echo "deb http://us.archive.ubuntu.com/ubuntu/ xenial main" | sudo tee -a /etc/apt/sources.list
     echo "deb http://us.archive.ubuntu.com/ubuntu/ xenial universe" | sudo tee -a /etc/apt/sources.list
@@ -12,4 +17,9 @@ else
     sudo apt-get update -qq
     sudo apt-get install build-essential libtool autotools-dev automake libmicrohttpd-dev
     sudo apt-get install libcurl4-gnutls-dev nettle-dev libjson-c-dev libuv1-dev
+    git clone https://github.com/Storj/libstorj.git
+    cd libstorj
+    ./autogen.sh
+    ./configure --prefix=/usr
+    sudo make install
 fi
