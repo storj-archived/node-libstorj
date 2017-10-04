1d8e4ca7801b1ab3958decb63de96e681f5f07a1
Braydon Fuller <courier@braydon.com>
2017-10-04 01:09:31 -0400

    Add .travis.sh install file

---
+++ .travis.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    brew install libtool automake libmicrohttpd pkgconfig
+    brew install curl nettle json-c libuv
+else
+    echo "deb http://us.archive.ubuntu.com/ubuntu/ xenial main" | sudo tee -a /etc/apt/sources.list
+    echo "deb http://us.archive.ubuntu.com/ubuntu/ xenial universe" | sudo tee -a /etc/apt/sources.list
+    echo "deb http://us.archive.ubuntu.com/ubuntu/ xenial-updates universe" | sudo tee -a /etc/apt/sources.list
+    echo "deb http://us.archive.ubuntu.com/ubuntu/ xenial-updates multiverse" | sudo tee -a /etc/apt/sources.list
+    echo "deb http://security.ubuntu.com/ubuntu xenial-security main" | sudo tee -a /etc/apt/sources.list
+    echo "deb http://security.ubuntu.com/ubuntu xenial-security multiverse" | sudo tee -a /etc/apt/sources.list
+    sudo apt-get update -qq
+    sudo apt-get install build-essential libtool autotools-dev automake libmicrohttpd-dev
+    sudo apt-get install libneon27-gnutls-dev nettle-dev libjson-c-dev libuv1-dev
+fi
