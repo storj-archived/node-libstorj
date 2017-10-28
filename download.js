c5fe15d57f195ba3307d9da15c5bc219e200a9be
sage.xue <job.xueqi@gmail.com>
2017-10-28 14:58:28 +0800

    Update download.js

--- download.js
+++ download.js
@@ -44,7 +44,7 @@ if (!filename) {
 const url = baseUrl + '/' + filename;
 const target = path.resolve(basedir, './' + filename);
 const download = `curl --location --fail --connect-timeout 120 --retry 3 -o "${target}" "${url}"`
-const extract = `expand.exe ${target} .\`;
+const extract = `expand.exe ${target} .\`
 const hasher = `${sha256sum} ${target} | awk '{print $1}'`
 
 if (fs.existsSync(target)) {
